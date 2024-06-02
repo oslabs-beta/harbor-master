eeb10a19c9c55366d4cb966bed502faa7409b863
Najm Safar <najmsafarjalani@gmail.com>
2024-06-01 21:30:31 -0400

    added pkg json file on root to run all app

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import App from './components/App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import './styles/style.css';
-
-const container = document.getElementById('app-root')!;
+const container = document.getElementById('app-root');
 const root = createRoot(container);
-root.render(<App></App>);
+root.render(React.createElement(App, null));
+//# sourceMappingURL=index.js.map
