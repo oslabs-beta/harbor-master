ffa12f19cc57a19b8784f8bb1b3b29b585c0a0bc
tristanfrank <TRISTAN1FRANK@GMAIL.COM>
2024-06-08 11:53:39 -0600

    Mongo schema setup
    - Added schemas
    - Added interfaces and types in root directory to be used in both frontend and backend
    - Edited tsconfig.json files to allow for absolute file paths to reach interface and type definitions

---
+++ server/src/db/schemas/VertexSchema.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import Vertex from 'interfaces/Vertex';
+
+const Schema = mongoose.Schema;
+
+const VertexSchema = new Schema<Vertex>({
+  resourceType: String,
+  position: [Number],
+  data: Object
+});
+
+module.exports = VertexSchema;
