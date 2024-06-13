956c675aec08591ad7a56a07b74bf602592f85c2
Najm Safar <najmsafarjalani@gmail.com>
2024-06-12 22:52:47 -0400

    total folder restructure

--- interfaces/VertexData.ts
+++
@@ -1,4 +0,0 @@
-export default interface VertexData {
-  /* placeholder interface - 
-  might also need differently defined interfaces for each K8s resource type */
-}
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req: Request, res: Response, next: NextFunction): void => {
+  try {
+    res.send('index.html');
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/', (req: Request, res: Response) => {
+  res.json({ AppName: 'Master-Harbor' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
