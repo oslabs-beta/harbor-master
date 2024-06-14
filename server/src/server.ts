cc86b938d0102efdf167a7a096ab45f9dd87dfaf
tristanfrank <TRISTAN1FRANK@GMAIL.COM>
2024-06-13 20:36:44 -0600

    Initial setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 // const express = require('express');
 import express, { Express, Request, Response } from 'express';
+import authController from './controllers/authController';
+
 const port = 8080;
 const app: Express = express();
 
-app.get('/api/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
   res.json({ AppName: 'Master-Harbor' });
 });
+
+app.get('/github-login', authController.githubLogin)
+
+app.get('/auth-callback', authController.callback);
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
