9fa7992874f859622d727c49daa2e5ee2bdeed21
Ashraf Khan <ashrafkhaneetli@gmail.com>
2024-07-23 19:35:48 -0400

    with react google charts

--- dist3/client/hooks.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useAppSelector = exports.useAppDispatch = void 0;
-var react_redux_1 = require("react-redux");
-// Use throughout app instead of plain `useDispatch` and `useSelector`
-exports.useAppDispatch = react_redux_1.useDispatch;
-exports.useAppSelector = react_redux_1.useSelector;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import App from './App';
-import ChartsOverviewDemo from './ChartsOverviewDemo'
+import { App } from './App';
+
 
 
 
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       
-      <ChartsOverviewDemo/>
+      <App/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('app')
