fa88a5b819ef605a29a7454c6293c57ce2db2aa5
tristanfrank <TRISTAN1FRANK@GMAIL.COM>
2024-07-23 19:08:08 -0600

    Added GitHub API, error handling

--- src/common/interfaces/AuthController.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export default interface AuthController {
-  githubLogin: (req: Request, res: Response, next: NextFunction) => void
-  callback: (req: Request, res: Response, next: NextFunction) => Promise<void>
-}
