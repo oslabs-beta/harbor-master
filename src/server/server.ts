6f523cb330200804f46eb62c67403a3cc115625f
tristanfrank <TRISTAN1FRANK@GMAIL.COM>
2024-07-23 19:08:08 -0600

    Added GitHub API, error handling

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,34 +1,59 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import path from 'path';
+import cookieParser from 'cookie-parser';
 import config from './config/envConfig';
 import { createProject, getProjectById, editProjectState } from './controllers/projectController';
+import { githubLogin, callback, getRepositories, getUser, verifyUser } from './controllers/userController';
+import { handleError } from './controllers/errorController';
 import UploadService from './services/UploadService';
+import { ProjectModel } from './config/mongoConfig';
 
 const app = express();
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../../public')));
+
 const uploadService = new UploadService();
 const uploadFileMiddleware = uploadService.generateUploadMiddleware();
 
-app.use(express.static(path.join(__dirname, '../../public')));
-
-app.get('/', (req: Request, res: Response, next: NextFunction): void => {
+app.get('/', (req, res, next) => {
   try {
     res.send('index.html');
   } catch (error) {
-    next(error);
+    return next({ log: 'Error sending index.html to client', message: { err: 'Server error loading page' } });
   }
 });
 
-app.get('/api', (req: Request, res: Response) => {
+app.get('/api', (req, res) => {
   res.json({ AppName: 'Master-Harbor' });
 });
 
+// project controller
 app.post('/create-project', uploadFileMiddleware, createProject, (req, res) => {
-  res.status(200).json( { id: res.locals.id })
+  res.json({ id: res.locals.id })
 });
-
 app.get('/get-project/:id', getProjectById, (req, res) => {
-  res.status(200).json(res.locals.project)
+  res.json(res.locals.project)
 })
+
+// auth controller
+app.get('/login', githubLogin);
+app.get('/auth-callback', callback);
+app.get('/get-repos', getRepositories, (req, res) => {
+  res.json(res.locals.repos)
+});
+app.get('/get-user', getUser, verifyUser, (req, res) => {
+  res.json(res.locals.user);
+})
+
+// mock endpoint to check db
+app.get('/read-db', async (req, res) => {
+  const response = await ProjectModel.find();
+  res.json(response);
+})
+
+// global error handling
+app.use(handleError);
 
 app.listen(config.port, () => {
   console.log(`App listening on port ${config.port}`);
