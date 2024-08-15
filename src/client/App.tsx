e6d1765a4b487f2c0d7ae640c55a6baf1c605082
Ashraf Khan <ashrafkhaneetli@gmail.com>
2024-08-14 22:33:45 -0400

    naj ui added second commit

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,3 +1,47 @@
+// import React from 'react';
+// import Navbar from './Navbar';
+// import Container from './Container';
+// import SideNav from './Side-nav';
+// import Banner from './Banner';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Home from './Home';
+// import Account from './Account';
+// import Deployments from './Deployments';
+// import Metrics from './Metrics';
+
+// const App: React.FC = () => {
+//   return (
+//     <BrowserRouter>
+//       <>
+//         <Navbar />
+//         {/* <Banner /> */}
+//         <div className='flex'>
+//           <SideNav />
+//           <Container>
+//             <Routes>
+//               <Route path='/' element={<Home />}></Route>
+//               <Route path='/metrics' element={<Metrics />}></Route>
+//               <Route path='/deployments' element={<Deployments />}></Route>{' '}
+//               <Route path='/account' element={<Account />}></Route>
+//             </Routes>
+//           </Container>
+//         </div>
+//       </>
+//     </BrowserRouter>
+//   );
+// };
+
+// export function App() {
+//   return (
+//     <Chart
+//       chartType="Line"
+//       width="100%"
+//       height="400px"
+//       data={data}
+//       options={options}
+//     />
+//   );
+// }
 import React from 'react';
 import Navbar from './Navbar';
 import Container from './Container';
@@ -31,14 +75,4 @@ const App: React.FC = () => {
   );
 };
 
-export function App() {
-  return (
-    <Chart
-      chartType="Line"
-      width="100%"
-      height="400px"
-      data={data}
-      options={options}
-    />
-  );
-}
+export default App;
