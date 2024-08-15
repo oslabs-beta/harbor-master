848c9195fdaf08cfab8f48cd74adb74a2c7cffab
Ashraf Khan <ashrafkhaneetli@gmail.com>
2024-08-15 18:35:31 -0400

    initial commit

---
+++ src/client/Cores.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+import { useState, useEffect } from "react";
+
+export function Cores(props:any) {
+    const [data, setData] = useState([[]]);
+
+    useEffect(() => {
+      fetch('/metrics/cores/' + props.from + '/' + props.from)
+        .then(res => res.json())
+        .then(data => {
+            
+            setData(data);
+        
+    })
+    .catch(e => console.log('error retreiving cores data from back end server'));
+    },[props]);
+
+      const options = {
+        chart: {
+          title: "Cores Per Instance",
+          subtitle: "_____",
+        },
+      };
+
+  return (
+    <Chart
+      chartType="Table"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
