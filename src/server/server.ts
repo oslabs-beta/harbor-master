f27dcdad5820ad27d0acd96de67789e2b1175f76
crossur <cruz24955@gmail.com>
2025-01-12 22:34:08 -0500

    main

--- src/server/server.ts
+++ src/server/server.ts
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, '../../public')));
 //   next();
 // });
 
-app.use(cors({origin: `http://localhost:3000`,}));
+app.use(cors({origin: `http://34.71.203.49:3000`,}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
